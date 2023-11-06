1d379159639860c2bec5a0c444b203b625fb983a
Soroush <s.ghodrat@gmail.com>
2023-11-06 16:11:07 +0100

    Format code

--- src/components/DataList.tsx
+++ src/components/DataList.tsx
@@ -1,26 +1,26 @@
-import generateRandomData from "../helper/DataGenerator";
-import "../App.css";
-import { useState } from "react";
+import generateRandomData from '../helper/DataGenerator';
+import '../App.css';
+import { useState } from 'react';
 
 const DataList = () => {
   const [value, setValue] = useState(1);
   const [error, setError] = useState<string | null>(null);
   const [data, setData] = useState(generateRandomData());
-  
+
   const dataString = JSON.stringify(data, null, 2);
-  const dataBlob = new Blob([dataString], { type: "application/json" });
+  const dataBlob = new Blob([dataString], { type: 'application/json' });
   const dataUrl = URL.createObjectURL(dataBlob);
 
   const handleCopy = async () => {
     await navigator.clipboard.writeText(dataString);
-    alert("Data copied to clipboard");
+    alert('Data copied to clipboard');
   };
 
   const handleNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
     const insertValue = event.target.value;
     if (Number(insertValue) < 1 || !Number.isInteger(Number(insertValue))) {
-      setError("Please enter a whole number equal or greater than 1");
+      setError('Please enter a whole number equal or greater than 1');
     } else {
       setError(null);
       setValue(Number(insertValue));
@@ -40,13 +40,7 @@ const DataList = () => {
       </span>
 
       <div className="generate">
-        <input
-          type="number"
-          min="1"
-          placeholder="Enter a number"
-          required
-          onChange={handleNumberChange}
-        />
+        <input type="number" min="1" placeholder="Enter a number" required onChange={handleNumberChange} />
         <button onClick={handleGenerateData}>GENERATE</button>
       </div>
 
--- src/helper/DataGenerator.ts
+++ src/helper/DataGenerator.ts
@@ -1,12 +1,5 @@
-import Data from "../declarations/models";
-import {
-  maleFirstNames,
-  femaleFirstNames,
-  lastNames,
-  cities,
-  positions,
-  genders,
-} from "../mocks/data";
+import Data from '../declarations/models';
+import { maleFirstNames, femaleFirstNames, lastNames, cities, positions, genders } from '../mocks/data';
 
 export default function generateRandomData(count = 1): Array<Data> {
   const data = [];
@@ -16,16 +9,14 @@ export default function generateRandomData(count = 1): Array<Data> {
       randomGender === 'male'
         ? maleFirstNames[Math.floor(Math.random() * maleFirstNames.length)]
         : femaleFirstNames[Math.floor(Math.random() * femaleFirstNames.length)];
-    const randomLastName =
-      lastNames[Math.floor(Math.random() * lastNames.length)];
-    const randomPosition =
-      positions[Math.floor(Math.random() * positions.length)];
+    const randomLastName = lastNames[Math.floor(Math.random() * lastNames.length)];
+    const randomPosition = positions[Math.floor(Math.random() * positions.length)];
     const randomCity = cities[Math.floor(Math.random() * cities.length)];
 
     data.push({
       _id: Math.random().toString(36).substr(2, 9),
       isAvailable: Math.random() < 0.5,
-      picture: "http://placehold.it/32x32",
+      picture: 'http://placehold.it/32x32',
       age: Math.floor(Math.random() * (70 - 22 + 1)) + 22,
       first_name: randomFirstName,
       last_name: randomLastName,
